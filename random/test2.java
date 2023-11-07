7fbd160b31173ddbe220365ab8c93f96c8b294a8
prateeeksahu <100373713+prateeeksahu@users.noreply.github.com>
2023-11-08 00:21:04 +0530

    "081123"

--- random/test2.java
+++ random/test2.java
@@ -2,14 +2,29 @@ package practice.random;
 import java.util.*;
 public class test2 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 11;
 
+        for (int i = 0; i <=n/2; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == n / 2 - i || j == n / 2 + i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
-        for(int i = 0;i<n;i++){
-            for (int j = 5;j>i;j--){
-                System.out.print(j);
+        for (int i = n/2-1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                if (j == n / 2 - i || j == n / 2 + i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
     }
+
 }
