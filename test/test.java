6f7c1d6cac7afadd495545332b5d30d17d44dde1
prateeeksahu <100373713+prateeeksahu@users.noreply.github.com>
2024-02-11 22:57:45 +0530

    "110224"

--- test/test.java
+++ test/test.java
@@ -44,7 +44,6 @@ public class test {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         String str = sc.nextLine();
 
         for (int i = 0; i < str.length(); i++) {
