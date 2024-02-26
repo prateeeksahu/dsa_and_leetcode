472bb60408d09aa82d76be94ed682750c8d76e9e
prateeeksahu <100373713+prateeeksahu@users.noreply.github.com>
2024-02-27 00:20:50 +0530

    "270224"

--- test/test2.java
+++ test/test2.java
@@ -1,58 +1,38 @@
+
+
 package practice.test;
-//public class test2 {
-//    public static void main(String[] args) {
-//
-//        Scanner sc = new Scanner(System.in);
-//        String str = sc.nextLine();
-//
-//        solve(str);
-//
-//    }
-//
-//    public static void solve(String str){
-//
-//        if(str.length() == 1 && str == "1") { System.out.println("YES");} else { System.out.println("NO");}
-//        if(str.length() == 2 && str == "14") { System.out.println("YES");} else { System.out.println("NO");}
-//        if(str.length() == 3 && str == "144") { System.out.println("YES");} else { System.out.println("NO");}
-//
-//        int i = 0;
-//        int j = 2;
-//        boolean mn = true;
-//
-//        while (j < str.length()) {
-//            String substr = str.substring(i,j+1);
-//
-//            for(int k = 0; k<3;k++){
-//
-//                if(i == 0 && substr.substring(0,1) != "1") mn = false;
-//                if(i == 1 && substr.substring(0,2) != "14") mn = false;
-//                if(i == 2 && sxubstr.substring(0,3) != "144") mn = false;
-//            }
-//
-//            i++; j++;
-//        }
-//
-//        if (mn) System.out.println("YES");
-//        else System.out.println("NO");
-//
-//    }
-//}
-//
-
 import java.util.*;
-public class test2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
 
-        if (isMagicNumber(str)) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
+public class test2 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int t = sc.nextInt();
+        while (t > 0) {
+            System.out.println(solve());
+            t--;
         }
     }
 
-    public static boolean isMagicNumber(String str) {
-        return str.matches("^(1|14|144)*$");
+    public static int solve() {
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int n = sc.nextInt();
+
+        while(n>=0){
+            int r = n % x;
+            if(r>=y){
+                int d = r-y;
+                return n-d;
+            } else {
+                n = n - (r + 1);
+                r = n % x;
+            }
+        }
+
+        return 0;
     }
 }
+
