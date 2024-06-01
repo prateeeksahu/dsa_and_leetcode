b2f7c1468be854572ade96187f916ce4733f09cb
prateeeksahu <prateekshahu9422@gmail.com>
2024-06-01 19:41:52 +0530

    010624

--- test/test.java
+++ test/test.java
@@ -1,70 +1,28 @@
 package practice.test;
-
 import java.util.*;
 
 public class test {
-    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
+        int [] quality = {3,1,10,10,1};
+        int [] wage = {4,8,2,2,7};
 
-        int t = sc.nextInt();
-
-        while(t>0){
-
-            solve();
-
-
-            t--;
+        int n = quality.length;
+        double[] ratios = new double[n];
+        for (int i = 0; i < n; i++) {
+            ratios[i] = (double) wage[i] / quality[i];
         }
 
-
-
-    }
-
-    public static void solve(){
-        int n = sc.nextInt();
-        int M = sc.nextInt();
-
-
-        int [] arr = new  int [n];
-        int prod = 1;
-        int l = 0;
-        int r = 0;
-        int [] ans = new int[n];
-
-
-        for(int i = 0; i<n; i++){
-            int x = sc.nextInt();
-            arr[i] = x;
+        Integer[] sortedIndices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            sortedIndices[i] = i;
         }
 
-        String com = sc.next();
+        Arrays.sort(sortedIndices, (idx1, idx2) -> Double.compare(ratios[idx1], ratios[idx2]));
 
-        for(int i = 0; i<com.length(); i++){
-
-            if(com.charAt(i) == 'L') l++;
-        }
-        r=l;
-
-        for(int i = com.length() -1 ; i >= 0; i--){
-            char ch = com.charAt(i);
-
-
-
-            if (ch == 'L') {
-                prod = (prod * arr[--l]) % M;
-            } else {
-                prod = (prod * arr[r++]) % M;
-            }
-
-            ans[i] = prod;
-        }
-
-        for(int a : ans){
-            System.out.print(a + " ");
-        }
-
+        for(Integer x : sortedIndices) System.out.print(x + " ");
         System.out.println();
+        for(double x : ratios) System.out.print(x + " ");
 
     }
 
--- test/test2.java
+++ test/test2.java
@@ -3,34 +3,9 @@ import java.util.*;
 
 public class test2 {
 
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
 
-        int t = sc.nextInt();
-        while (t > 0) {
-            System.out.println(solve());
-            t--;
-        }
-    }
-
-    public static int solve() {
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int n = sc.nextInt();
-
-        while(n>=0){
-            int r = n % x;
-            if(r>=y){
-                int d = r-y;
-                return n-d;
-            } else {
-                n = n - (r + 1);
-                r = n % x;
-            }
-        }
-
-        return 0;
+        System.out.println('r' - 'j');
     }
 }
 
