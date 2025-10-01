95bd2fffabb12e484458aed99e42f0f400452341
prateek <pratiksatishsahu@Pratiks-MacBook-Air.local>
2025-10-02 01:05:03 +0530

    011025

--- algorithmns_and_concepts/algorithmns_and_concepts/prep/prep_code/abstract_classes/exceptionHandling/trycatch.java
+++ algorithmns_and_concepts/algorithmns_and_concepts/prep/prep_code/abstract_classes/exceptionHandling/trycatch.java
@@ -1,27 +1,36 @@
 package algorithmns_and_concepts.algorithmns_and_concepts.prep.prep_code.abstract_classes.exceptionHandling;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
 public class trycatch {
 
-    private static int divide(int a, int b){
-        try {
-            return a/b;
-        } catch(Exception e){
-            System.out.println(e);
-            e.getStackTrace();
+//    private static int divide(int a, int b){
+//        try {
+//            return a/b;
+//        } catch(Exception e){
+//            System.out.println(e);
+//            e.getStackTrace();
+//
+//        }
+//
+//            return -1;
+//        }
+//    }
 
-        }
 
-            return -1;
-        }
-    }
 
     public static void main(String[] args) {
-        int[] arr1 = new int[]{1,2,3, 4};
-        int[] arr = new int[]{47,985,0, 858};
+//        int[] arr1 = new int[]{1,2,3, 4};
+//        int[] arr = new int[]{47,985,0, 858};
+//
+//        for(int i = 0;i<4; i++) System.out.println(trycatch.divide(arr1[i], arr[i]));
+//
+//        Integer a = 1;
 
-        for(int i = 0;i<4; i++) System.out.println(trycatch.divide(arr1[i], arr[i]));
-
-        Integer a = 1;
-
+        ConcurrentSkipListSet<Integer> s = new ConcurrentSkipListSet<>();
     }
 }
---
+++ algorithmns_and_concepts/algorithmns_and_concepts/prep/prep_code/abstract_classes/generics/generics.java
@@ -0,0 +1,30 @@
+package algorithmns_and_concepts.algorithmns_and_concepts.prep.prep_code.abstract_classes.generics;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import static java.lang.StringUTF16.compareTo;
+
+
+abstract class hello implements Comparable{
+    public hello(){
+        super();
+    }
+}
+
+public class generics {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList(1000);
+
+        list.sort(Comparator.comparing())
+        list.add("hello");
+        list.add(123); // throws error now
+
+        ArrayList<Integer> li = new ArrayList(1000);
+
+        li.remove((Integer) 1);
+
+      compareTo();
+    }
+}
